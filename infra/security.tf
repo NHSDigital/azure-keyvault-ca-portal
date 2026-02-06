dfb58d27653761009d7abb44e22668a43d0035ac
Marcel du Preez <dupreez.marcel@gmail.com>
2026-02-06 13:33:05 +0000

    fix(infra): correct key vault rbac argument

--- infra/security.tf
+++ infra/security.tf
@@ -10,7 +10,7 @@ resource "azurerm_key_vault" "kv" {
   resource_group_name           = azurerm_resource_group.rg.name
   tenant_id                     = data.azurerm_client_config.current.tenant_id
   sku_name                      = "standard"
-  rbac_authorization_enabled    = true
+  enable_rbac_authorization     = true
   public_network_access_enabled = false
   #soft_delete_retention_days    = 7
   purge_protection_enabled = false
